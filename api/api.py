a23d3b65ca184b3daaac92c1b48e391f5acc2630
Eliot <eliot.meurillon@gmail.com>
2022-03-22 15:24:20 +0100

    v2

--- api/api.py
+++ api/api.py
@@ -1,4 +1,4 @@
-import time
+from datetime import datetime
 from flask import Flask
 
 app = Flask(__name__)
@@ -6,4 +6,6 @@ app = Flask(__name__)
 
 @app.route('/api/time')
 def get_current_time():
-    return {'time': time.time()}
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    return {'time': current_time}
